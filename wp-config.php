9206d653d624501c1b23102b678db5ac1ff179e9
zhangquanle <setsaille@gmail.com>
2016-04-13 11:50:53 +0800

    修改连接

--- wp-config.php
+++ wp-config.php
@@ -15,16 +15,16 @@
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
 /** WordPress数据库的名称 */
-define('DB_NAME', 'zhangquanle.com');
+define('DB_NAME', 'zhangquanle');
 
 /** MySQL数据库用户名 */
-define('DB_USER', 'zhangquanle');
+define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', 'zql12345qwert');
+define('DB_PASSWORD', 'SAIL1208set');
 
 /** MySQL主机 */
-define('DB_HOST', 'zhangquanle.com');
+define('DB_HOST', '192.168.0.2');
 
 /** 创建数据表时默认的文字编码 */
 define('DB_CHARSET', 'utf8');
