b4ccd593bd61086898b633ebd39ec4d688901f35
zhangquanle <setsaille@gmail.com>
2016-05-09 16:29:23 +0800

    修改footer

--- wp-content/themes/9iphp/footer.php
+++ wp-content/themes/9iphp/footer.php
@@ -2,12 +2,27 @@
 </div>
 <footer id="body-footer">
     <div  class="container clearfix">
-        Copyright © 2014 <?php bloginfo('name'); ?> |
-        <?php $site_analytics = of_get_option('site_analytics', false); if($site_analytics){ echo (strpos($site_analytics, '<script') === false) ? '<script>'.$site_analytics.'</script> | ' : $site_analytics; } ?>
-        Theme By <a href="http://9iphp.com" title="Specs' Bolg" target="_blank">Specs</a>
+        Copyright &nbsp;&nbsp; ©2015&nbsp;&nbsp;
+      <a href="http://zhangquanle.com/"  target="_blank"><?php echo stripslashes( get_option( 'zan_footer' ) ); ?></a> 
+     &nbsp;&nbsp; 版权所有 &nbsp;&nbsp;
+      <a href="http://www.miitbeian.gov.cn/"  target="_blank">京ICP备14056998号</a>
+          <!--统计代码开始-->
+          <?php $analytics = get_option( 'zan_analytics' );if ( $analytics != "" ) : ?>
+            <?php echo stripslashes( $analytics ); ?>
+          <?php endif ?>
+      <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?af46dd6f6caa13922bc3ff318387a37a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+          <!--统计代码结束-->
     </div>
     <ul id="jump" class="visible-lg">
-        <li><a id="share" title="意见反馈" href="<?php bloginfo('url'); ?>/guestbook" target="_blank"><i class="fa fa-comments-o"></i></a></li>
+        <li><a id="share" title="意见反馈" href="mailto:setsaille@gmail.com" target="_blank"><i class="fa fa-comments-o"></i></a></li>
         <li><a id="top" href="#top" title="返回顶部" style="display:none;"><i class="fa fa-arrow-circle-up"></i></a></li>
     </ul>
 </footer>
